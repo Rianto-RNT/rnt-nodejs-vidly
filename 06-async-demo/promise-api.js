0fa3633e3301494bc42bd94388d659818e8b361b
Rianto <rianto.rnt@gmail.com>
2022-08-15 21:16:33 +0800

    running parallel promises

--- 06-async-demo/promise-api.js
+++ 06-async-demo/promise-api.js
@@ -1,2 +1,20 @@
-const p = Promise.reject(new Error("reason fo rejection..."));
-p.catch((error) => console.log(error));
+// const p = Promise.reject(new Error("reason fo rejection..."));
+// p.catch((error) => console.log(error));
+
+const promiseOne = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 1...");
+    resolve(1);
+  }, 2000);
+});
+
+const promiseTwo = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 2...");
+    resolve(2);
+  }, 2000);
+});
+
+Promise.race([promiseOne, promiseTwo])
+  .then((result) => console.log(result))
+  .catch((err) => console.log("Error", err.message));
